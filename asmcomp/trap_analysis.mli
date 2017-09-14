fa31069bf2c882fd4457d6b61eff9a24c2cf7f71
Vincent Laviron <vincent.laviron@gmail.com>
2017-09-14 14:15:06 +0200

    Fixes for closure

---
+++ asmcomp/trap_analysis.mli
@@ -0,0 +1,23 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*           Mark Shinwell and Leo White, Jane Street Europe              *)
+(*                                                                        *)
+(*   Copyright 2016 OCamlPro SAS                                          *)
+(*   Copyright 2016 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Fill in the [trap_stack] members of Mach instructions by calculating
+    which exception handlers are in scope for each instruction.
+
+    This pass also removes unused [Icatch] handlers.
+*)
+
+val run : Mach.fundecl -> Mach.fundecl
